938dd221986b9631ce86f694a17bc7b7969e5254
NizamutdinovEmir <144460470+NizamutdinovEmir@users.noreply.github.com>
2024-04-15 23:18:47 +0300

    Add files via upload

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "C:\labwork5\lib\BitArray3D.cpp"
+int main() {
+  ThreeArray arr = ThreeArray::make_array(3, 3, 3);
+  int value = 0;
+  for (size_t i = 0; i < 3; ++i) {
+    for (size_t j = 0; j < 3; ++j) {
+      for (size_t k = 0; k < 3; ++k) {
+        arr[i][j][k] = value;
+        value++;
+      }
+    }
+  }
+  ThreeArray ans = arr * 2;
+  ThreeArray ans2 = ans + arr;
+  std::cout << ans2;
+  return 0;
 
+}
